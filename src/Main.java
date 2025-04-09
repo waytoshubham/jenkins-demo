a2981a17ae4b2d248666b284a820399071401371
waytoshubham <waytoshubham3043@gmail.com>
2025-04-09 18:56:20 +0530

    Testing ... !!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins on Windows!");
+        System.out.println("Hello from Jenkins and Docker on Windows!");
     }
 }
 // Test purpose
